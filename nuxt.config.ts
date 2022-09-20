5f7f2da2fff12c19bf4d06f4412a7e7985ed9712
genuinely <ola.holmgren@gmail.com>
2022-09-20 11:57:07 +0200

    pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config.js',
+        exposeConfig: false,
+        config: {},
+        injectPosition: 0,
+        viewer: true,
+      },
+      colorMode: {
+       classSuffix: ''
+      },
 })
