39407d2b5e83d5612d72557ebde0886352e8523b
supapow <ola.holmgren@gmail.com>
2022-11-10 20:06:04 +0100

    Setup step 1

--- pages/setup.vue
+++ pages/setup.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="bg-black text-white w-full h-full flex pb-16">
+  <div class="bg-black text-white w-screen h-screen flex justify-center">
 
     <!-- STEP 1 -->
 
-    <div v-if="step1" class="z-10 step1 w-full">
+    <div v-if="step1" class="sm:h-80 w-[80%] z-10 step1 flex flex-col justify-center mb-24">
 
       <!-- left -->
-      <div class="mt-56 ml-24 absolute text-slate-600 text-lg">
-        Account Setup: Step 1/3
-      </div>
-      <div class="mt-64 ml-24 absolute text-white0 text-3xl">
-        Let's get to know you!
+
+      <div class="mb-20 h-[10vh] w-full flex flex-col items-start">
+        <div class="md:mt-56 md:ml-24 md:absolute text-slate-600 text-lg">
+          Account Setup: Step 1/3
+        </div>
+        <div class="md:mt-64 md:ml-24 md:absolute text-white0 text-3xl">
+          Let's get to know you!
+        </div>
       </div>
 
-      <button class="z-10 absolute top-[202px] left-[680px] w-44 flex justify-end" @click="goToStep2">
-          <svg class="-rotate-90" width="92" height="111" viewBox="0 0 92 159" fill="none"
+      <button class="z-10 w-14 absolute bottom-[140px] right-12 md:top-[202px] md:left-[680px] md:w-44 flex justify-end" @click="goToStep2">
+          <svg class="-rotate-90 w-10 h-14 md:w-20 md:h-28" viewBox="0 0 92 159" fill="none"
             xmlns="http://www.w3.org/2000/svg">
             <path
               d="M40.7216 0L43.7377 140.969C43.7377 140.969 37.2022 108.732 2.81487e-05 108.732C5.42783e-05 112.557 -4.19512e-05 114.742 4.39703e-05 118.627C46.2514 118.627 44.7431 159 44.7431 159H46.2514C46.2514 159 46.2514 118.567 92 118.567C92 114.742 92 112.557 92 108.732C55.3006 108.732 47.7595 140.969 47.7595 140.969L50.2735 0H40.7216Z"
@@ -22,39 +25,37 @@
           </svg>
       </button>
       
-      <div class="h-full w-full z-10 flex">
-        <div class="mt-32 ml-24 w-4/5 text-6xl flex items-center">
-          <div class="h-72 w-full text-white flex flex-col">
-            <div>
-              <span class="mr-8">My name is</span>
+      <div class="h-[23vh] md:h-full z-10 flex justify-center">
+        <div class="text-1xl md:mt-32 md:ml-24 md:text-6xl items-center flex flex-col justify-between">
+
+            <div class="flex w-full justify-between items-center">
+              <span class="w-4/12">My name is</span>
               <input v-model="userInfo.name" type="text"
-                    class="text-center w-96 mr-8 outline-[0px] bg-transparent h-24 border-b-white border-b-2 px-0 text-white placeholder-white"
-                    placeholder="" />
+                    class="text-center text-2xl h-11 w-8/12 md:w-96 outline-[0px] bg-transparent md:h-24 border-b-[rgba(255,255,255,0.12)] border-b-2 px-0 text-white placeholder-[rgba(255,255,255,0.06)]"
+                    placeholder="Cat McKitty" />
             </div>
-            <div>
-            <span class="work1 mr-8">and I work at</span>
+
+            <div class="flex w-full justify-between items-center">
+              <span class="work1 w-4/12">I work at</span>
             
-            <input v-model="userInfo.company" type="text"
-                    class="text-center work2 w-96 outline-[0px] bg-transparent h-24 border-b-white border-b-2 px-0 text-white placeholder-white"
-                    placeholder="" />
+              <input v-model="userInfo.company" type="text"
+                    class="text-center work2 text-2xl h-11 w-8/12 md:w-96 outline-[0px] bg-transparent md:h-24 border-b-[rgba(255,255,255,0.12)] border-b-2 px-0 text-white placeholder-[rgba(255,255,255,0.06)]"
+                    placeholder="The Milk Bowl Inc." />
             </div> 
-            <div>
-              <span class="team1 mr-8">in the</span>
+
+            <div class="flex w-full justify-between items-center">
+              <span class="team1 w-4/12">in the</span>
               <input v-model="userInfo.team" type="text"
-                    class="text-center team2 w-96 outline-[0px] bg-transparent h-24 border-b-white border-b-2 px-0 text-white placeholder-white"
-                    placeholder="" />
+                    class="text-center w-8/12 text-2xl h-11 team2 md:w-96 outline-[0px] bg-transparent md:h-24 border-b-[rgba(255,255,255,0.12)] border-b-2 px-0 text-white placeholder-[rgba(255,255,255,0.06)]"
+                    placeholder="Meow" />
               <span class="team1 mx-8">team.</span>
             </div>
-          </div>
-        </div>
 
-        <div class="w-1/5 flex mt-56 justify-end z-10">
+
         </div>
 
       </div>
-    </div>
-    <div v-if="step1" class="absolute h-full w-full bg-blue-400">
-      <div class="w-[930px] h-full bg-black"></div>
+
     </div>
     
 
@@ -62,14 +63,14 @@
 
     <div v-if="step2" class="z-10 step1 w-full">
 
-      <div class="mt-56 ml-24 absolute text-slate-600 text-lg">
+      <div class="mt-56 ml-24 md:absolute text-slate-600 text-lg">
         Account Setup: Step 2/3
       </div>
-      <div class="mt-64 ml-24 absolute text-white0 text-3xl">
+      <div class="mt-64 ml-24 md:absolute text-white0 text-3xl">
         Let's create your first project!
       </div>
 
-      <button class="z-10 absolute top-[202px] left-[680px] w-44 pr-7 flex justify-end" @click="goToStep3">
+      <button class="z-10 md:absolute top-[202px] left-[680px] w-44 pr-7 flex justify-end" @click="goToStep3">
         <svg class="-rotate-90" width="92" height="111" viewBox="0 0 92 159" fill="none"
             xmlns="http://www.w3.org/2000/svg">
           <path
@@ -100,14 +101,14 @@
 
     <div v-if="step3" class="z-10 step1 w-full">
 
-      <div class="mt-56 ml-24 absolute text-slate-600 text-lg">
+      <div class="mt-56 ml-24 md:absolute text-slate-600 text-lg">
         Account Setup: Step 3/3
       </div>
-      <div class="mt-64 ml-24 absolute text-white0 text-3xl">
+      <div class="mt-64 ml-24 md:absolute text-white0 text-3xl">
         Now, let's set up your project!
       </div>
 
-      <button class="z-10 absolute top-[202px] left-[680px] w-44 pr-7 flex justify-end" @click="goToStep1">
+      <button class="z-10 md:absolute top-[202px] left-[680px] w-44 pr-7 flex justify-end" @click="goToStep1">
         <svg class="-rotate-90" width="92" height="111" viewBox="0 0 92 159" fill="none"
           xmlns="http://www.w3.org/2000/svg">
           <path
@@ -137,10 +138,10 @@
 
       <div class="bgright z-0">
         <img src="/img/bg-right.svg" />
-        {{ userInfo.name }} , {{ userInfo.company }} , {{ userInfo.team }}
+        <!-- {{ userInfo.name }} , {{ userInfo.company }} , {{ userInfo.team }}
         <p> {{ userInfo }}</p>
         {{ projectInfo.name }}
-        <p> {{ projectInfo }}</p>
+        <p> {{ projectInfo }}</p> -->
       </div>
 
   </div>
