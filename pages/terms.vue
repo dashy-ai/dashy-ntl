c59cfaeabfb1781b72eb78aec715df9cfcb75547
supapow <ola.holmgren@gmail.com>
2022-11-29 18:06:24 +0100

    privacy policy and terms of service added

---
+++ pages/terms.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="pt-32 h-screen w-screen flex p-10 xl:p-48 text-white">
+    <div>
+      <div class="w-[90%] lg:w-[80%] xl:w-[70%] xl:pl-32 pb-48 tab-content translations-content-item en visible" id="en">
+      
+      <div class="medium-10 large-6">
+        <h1>Terms of service</h1>
+            <p>dashy.ai, Sweden (“<strong>dashy.ai</strong>”) has developed&nbsp;and is the owner of a web application available on dashy.ai. The application “<strong>app</strong>” is currently in Beta.<br/>
+            The app which can be used to create data visualizations and use artificial intelligence. The app enables an individual user to create, manage and share data visualizations &nbsp;(i.e., dashboards) to show and interpret data in a beautiful and iteresting way to capture interest of the audience.</p>
+            <p>These terms of service (“<strong>Terms</strong>”) apply to the use of the app. The Terms are an agreement between you (“<strong>You</strong>” or the “<strong>User</strong>”) and dashy.ai. By&nbsp;entering into an agreement with dashy.ai with reference to these Terms, or otherwise by using, creating an account and/or by logging&nbsp;onto app, You accept the Terms.</p>
+            <p><strong>1. Provision of Services.</strong></p>
+            <p><strong>1.1&nbsp;</strong>dashy.ai hereby grants to the User – during the&nbsp;agreed term – a non-exclusive right to use the app and to use the application  for agreed number of&nbsp;accounts. In addition, dashy.ai does not agree to&nbsp;provide support as requested by the User. But might, if possible provide support during the beta, and will provide support after the application moves out of Beta.</p>
+            <p><strong>1.2&nbsp;</strong>The app  may only be used&nbsp;for the agreed permitted use, i.e., unless otherwise is&nbsp;agreed and in addition to what follows from the&nbsp;general specification of the app. In addition, the app may not be used for illegal&nbsp;purposes or purposes that violate any applicable&nbsp;laws and regulations or for any other purposes that&nbsp;are or may be perceived as illegal, obscene, abusive,&nbsp;libellous, threatening, vulgar or otherwise can be&nbsp;regarded reprehensible.</p>
+            <p><strong>2. Fees and service charges</strong></p>
+            <p><strong>2.1&nbsp;</strong>dashy.ai is entitled to a set-up fee, a monthly&nbsp;license fees, a variable fee and, to the extent&nbsp;applicable, service charges, unless otherwise is&nbsp;agreed, according to dashy.ai’s price list&nbsp;applicable from time to time.</p>
+            <p><strong>2.2&nbsp;</strong>The set-up fee is payable to dashy.ai on agreed&nbsp;delivery date or otherwise when the User started to&nbsp;use the app. The monthly&nbsp;license fee shall be paid annually in advance against&nbsp;invoice during the agreed term with effect from the&nbsp;agreed delivery date (or otherwise when the User&nbsp;started to use the application) and shall be payable by&nbsp;the User within 20 days of receipt by the User of the &nbsp;invoice. The variable fee  charges, if&nbsp;applicable, shall be invoiced by dashy.ai on a&nbsp;monthly basis and shall be&nbsp;payable by the User within 20 days of receipt by the&nbsp;User of such invoice. dashy.ai reserves the right&nbsp;to charge the User a penalty interest of 12 per cent&nbsp;per annum or the highest rate permitted under&nbsp;applicable law (if higher) in respect of the late&nbsp;payment of any sum due from the due date until&nbsp;payment is made.</p>
+            <p><strong>2.3&nbsp;</strong>Unless otherwise stated, all amounts stated are&nbsp;expressed exclusive of value added tax. Value added&nbsp;tax and other charges levied on the fees payable by&nbsp;the User to dashy.ai shall, if applicable, be paid by&nbsp;the User.</p>
+            <p><strong>3. Service levels</strong></p>
+            <p>dashy.ai warrants that the app and the&nbsp;Services in all material respects will conform to the&nbsp;specifications and service levels from time to time&nbsp;undertaken by dashy.ai or as separately agreed&nbsp;upon.</p>
+            <p><strong>4. Proprietary rights</strong></p>
+            <p><strong>4.1</strong>&nbsp;dashy.ai warrants its title to and ownership of the&nbsp;app and that dashy.ai has the right,&nbsp;power and authority to assign or license the same&nbsp;upon agreed terms and conditions. Nothing in these&nbsp;Terms gives the User the right to use any of&nbsp;dashy.ai’s trade names, trademarks, logos,&nbsp;domain names or other distinguishing marks.</p>
+            <p><strong>4.2&nbsp;</strong>The User (or its licensors) owns all property rights to&nbsp;the data uploaded to or created using the&nbsp;app. dashy.ai obtains&nbsp;the right, title and interest from the User (or its&nbsp;licensors) in or to any content the User submit, post,&nbsp;transmit or display on or through the app&nbsp;. To obtain an open marketplace where Users can share templates and example data. No data owned or uploaded by a User will be shared from one licensor to another if not otherwise agreed or approved between or by licensors.</p>
+            <p><strong>5. Infringement and indemnity</strong></p>
+            <p><strong>5.1&nbsp;</strong>dashy.ai will indemnify the User for any damages,&nbsp;liabilities, losses, costs and expenses (including&nbsp;reasonable attorneys’ fees) that may be awarded to&nbsp;any third party based on the grounds that the app infringe or otherwise violate&nbsp;the intellectual property rights of a third party,&nbsp;except to the extent arising from the User’s use&nbsp;thereof in violation of agreed terms.&nbsp;dashy.ai shall have sole control over the defence&nbsp;and settlement of any indemnified claim and the&nbsp;User shall provide reasonable cooperation in&nbsp;connection therewith. The User may participate in&nbsp;the defence at its own expense, and all settlements&nbsp;require its consent (not to be unreasonably&nbsp;withheld).</p>
+            <p><strong>5.2&nbsp;</strong>It is the User’s responsibility to ensure that&nbsp;data uploaded to the app are&nbsp;legal, virus free and that they are in agreed format in&nbsp;order to be able to use the application.</p>
+            <p><strong>6. Limitation of liability</strong></p>
+            <p><strong>6.1&nbsp;</strong>The Parties’ liability to each other for any damage&nbsp;and/or cost resulting from breach of the contractual&nbsp;obligations and negligent actions or omissions shall&nbsp;be subject to the below limitations.</p>
+            <p><strong style="line-height: 1.5;">6.2&nbsp;</strong>Each party’s entire liability shall in any case not&nbsp;exceed an amount equal to two times the aggregate&nbsp;of the total fees paid or payable by the User under the agreed term. The Parties shall not be liable to&nbsp;each other hereunder for any lost profits, loss of&nbsp;anticipated savings nor any other type of indirect or&nbsp;consequential loss of any act or omission of the other&nbsp;party regardless of whether such losses were&nbsp;foreseeable or not.</p>
+            <p><strong>6.3&nbsp;</strong>&nbsp;The aforementioned limitations shall not apply in&nbsp;case of (i) fraudulent misrepresentation (ii) breaches&nbsp;of confidentiality obligations (Section 7) and (iii)&nbsp;breach of undertakings in Section 5, in which case&nbsp;the liability thus shall not be limited per above.</p>
+            <p><strong>6.4&nbsp;</strong>The aforementioned limitations shall further not&nbsp;apply to the User with regard to unpaid fees.</p>
+            <p><strong>6.5</strong> Except for the warranties expressly set forth in this&nbsp;agreement, dashy.ai expressly disclaims all other&nbsp;all warranties with respect to the application, including&nbsp;all statutory and implied warranties, all warranties of&nbsp;merchantability and fitness for a particular purpose&nbsp;or use (even if dashy.ai has been made aware of&nbsp;such purpose or use), all warranties that the application&nbsp;shall be uninterrupted or error free, all warranties of&nbsp;title and non-infringement, and all warranties arising&nbsp;from course of performance, course of dealing, or&nbsp;usage of trade, or their equivalents under the laws of&nbsp;any jurisdiction. Because some jurisdictions do not&nbsp;allow the exclusion of implied warranties, the above&nbsp;exclusion of implied warranties may not apply to&nbsp;you.</p>
+            <p><strong>7. Confidentiality</strong></p>
+            <p>Each of the Parties hereto undertakes to the other to&nbsp;keep confidential all information (written or oral),&nbsp;concerning the business and affairs of the other&nbsp;party that it has obtained or received as a result of&nbsp;the discussions leading up to, entrance into or&nbsp;performance of this agreement. The obligation to&nbsp;maintain the confidentiality of information obtained&nbsp;or received does not extend nor apply to information&nbsp;which at the time of the party’s disclosure is in the&nbsp;public domain or after such disclosure, becomes in&nbsp;the public domain otherwise than through an&nbsp;unauthorized disclosure by such party.</p>
+            <p><strong>8. Term and termination</strong></p>
+            <p><strong>8.1</strong>&nbsp;The right to use the app  is&nbsp;– unless otherwise is agreed separately – valid for an&nbsp;initial period of one year from agreed delivery date&nbsp;or otherwise from the date the User started to use&nbsp;the application . Unless the application is in Beta, or the agreement is terminated in writing at least&nbsp;three months prior to the expiry date, the agreement&nbsp;will thereafter be automatically extended for an&nbsp;indefinite period with a mutual right to terminate the&nbsp;agreement by giving six months’ prior notice in&nbsp;writing, except if the application is in Beta, then no such timeframe is valid from any of the parties.</p>
+            <p><strong>8.2&nbsp;</strong>In addition to the above, the agreement may&nbsp;immediately be terminated:</p>
+            <p>(a) by dashy.ai if the User fails to pay any&nbsp;sum due hereunder within 45 days of the&nbsp;due date of such payment;</p>
+            <p>(b) by either party if the other party commits&nbsp;a material breach, which (in the case of a&nbsp;breach capable of being remedied) shall&nbsp;not have been remedied within seven&nbsp;days following receipt of a written request&nbsp;to remedy the same from the non-&nbsp;breaching party; and</p>
+            <p>(c) by either party if the other party enters&nbsp;into liquidation or becomes bankrupt or&nbsp;insolvent or makes a composition with its&nbsp;creditors or generally fails to pay its debts&nbsp;as they become due.</p>
+            <p><strong>8.3</strong>&nbsp;At the written request of the User, at any time up&nbsp;until three months after termination of the&nbsp;agreement, dashy.ai shall return to the User,&nbsp;without further charges and in the format reasonably&nbsp;stipulated by the User, a copy of the User’s content&nbsp;uploaded on the app.</p>
+            <p><strong>9. Force majeure</strong></p>
+            <p><strong>9.1&nbsp;</strong>If a Force Majeure Event occurs, the party that is&nbsp;prevented by that Force Majeure event from&nbsp;performing any one or more obligations under this&nbsp;agreement (the “Nonperforming Party”) will be&nbsp;excused from performing those obligations, on&nbsp;condition that it complies with its obligations under&nbsp;section 9.3.</p>
+            <p><strong>9.2&nbsp;</strong>For purposes of this agreement, “Force Majeure&nbsp;Event” means, with respect to the Nonperforming&nbsp;Party, any event or circumstance, regardless of&nbsp;whether it was foreseeable or not, that was not&nbsp;caused by the Nonperforming Party and that&nbsp;prevents the Nonperforming Party from complying&nbsp;with any of its obligations under this agreement,&nbsp;provided that such Nonperforming Party uses&nbsp;reasonable efforts to do so.</p>
+            <p><strong>9.3&nbsp;</strong>Upon occurrence of a Force Majeure Event, the&nbsp;Nonperforming Party shall promptly notify the other&nbsp;party of occurrence of that Force Majeure Event, its&nbsp;effect on performance, and how long that the&nbsp;Nonperforming Party expects it to last. Thereafter&nbsp;the Nonperforming Party shall update that&nbsp;information as reasonably necessary. During a Force&nbsp;Majeure Event, the Nonperforming Party shall use&nbsp;reasonable efforts to limit damages to the other&nbsp;party and to resume its performance under this&nbsp;agreement.</p>
+            <p><strong>10. Applicable law and jurisdiction</strong></p>
+            <p><strong>10.1&nbsp;</strong>These Terms shall be governed by and construed in&nbsp;accordance with Swedish law.</p>
+            <p><strong>10.2 </strong>Any dispute, controversy or claim arising out of or in&nbsp;connection with this contract, or the breach,&nbsp;termination or invalidity thereof, shall be finally&nbsp;settled by arbitration administered by the Arbitration&nbsp;Institute of the Stockholm Chamber of Commerce&nbsp;(the “<strong>SCC</strong>”).</p>
+            <p>The Rules for Expedited Arbitrations shall apply,&nbsp;unless the SCC in its discretion determines, taking&nbsp;into account the complexity of the case, the amount&nbsp;in dispute and other circumstances, that the&nbsp;Arbitration Rules shall apply. In the latter case, the&nbsp;SCC shall also decide whether the Arbitral Tribunal&nbsp;shall be composed of one or three arbitrators. &nbsp;The seat of arbitration shall be Stockholm. The&nbsp;language to be used in the arbitral proceedings shall&nbsp;be English.</p>
+      </div>
+      
+      
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  
+</script>
+
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Archivo:wght@300&family=Barlow:wght@100&display=swap');
+
+/* font-family: 'Archivo', sans-serif;
+font-family: 'Barlow', sans-serif; */
+
+h1 {
+    padding-bottom: 40px; 
+    font-size: 30px;
+    padding-top: 30px;
+    font-family: 'Barlow', sans-serif;
+}
+
+p {
+    padding-bottom: 20px;
+    font-family: 'Barlow', sans-serif;
+}
+h2 {
+    padding-bottom: 10px;
+    font-size: 20px;
+    padding-top: 10px;
+    font-family: 'Barlow', sans-serif;
+}
+
+ul {
+  padding-bottom: 20px;
+  font-family: 'Barlow', sans-serif;
+}
+
+
+</style>
